2f4324a1f5462a122cfb8e50dfae5edbbcb819d3
hasanb69 <hasanbaratiasp@gmail.com>
2022-11-27 12:16:46 +0330

    update advance search

--- src/global/Chip/Chip.tsx
+++ src/global/Chip/Chip.tsx
@@ -1,53 +1,152 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {BiX, BiXCircle} from 'react-icons/bi'
 import {ServiceData} from "../../redux/ServiceDefine/ServiceDefineReducer";
 import {toast} from "react-toastify";
 import update = toast.update;
 
 
-interface chipData {
-    label?: string,
-    data?: any,
-    name: string,
-}
+
+
 interface propsData {
-    chipData: chipData[],
-    setChipData: React.Dispatch<React.SetStateAction<chipData[]>>,
-    formData:any
+
+
+    formData: any,
+    filterData: any,
+    // value?:any
+    // value:never
 }
 
-const Chip: React.FC<propsData> = ({chipData, setChipData,formData}: propsData) => {
+const Chip: React.FC<propsData> = ({filterData, formData}: propsData) => {
 
-    const handelClick = (item: any, id: number) => {
-        const findData = chipData.findIndex((item, index) => index === id);
-        chipData.splice(findData, 1)
-        formData.setFieldValue((item.name).toString(),'')  //empty value by name
+
+    const [chipData, setChipData] = useState<any>([]);
+    const handleRemoveChipData = ( id: number) => {
+        const findData = chipData.find((item: { id: number; }) => item.id === id);
+        const filterData = chipData.filter((item: { id: number; }) => item.id !== id);
+        const searchData = filterData.map((item: string) => item.search);
+        setChipData(filterData);
+        if (findData.mainTitle === "pelak" || findData.mainTitle === "vehicleNumber") {
+            formData.setFieldValue("vehicleNumber0", "");
+            formData.setFieldValue("vehicleNumber1", "");
+            formData.setFieldValue("vehicleNumber2", "");
+            formData.setFieldValue("vehicleNumber3", "");
+        }
+        formData.setFieldValue(Object.keys(findData?.search).toString(), "");
+        formData.handleSubmit(Object.assign({}, ...searchData));
     }
+
+
+    const persianName:any= {
+        code:  "کد هاب",
+        name:  "نام هاب",
+        hub: "نام هاب",
+        hubTypeId: "نوع هاب",
+        hubCategoryId: "گونه هاب",
+        parentHubId: "هاب والد",
+        bagNumber: "شماره کیسه",
+        selectBagType: "نوع کیسه",
+        sourceHub: "هاب مقصد",
+        destinationHub: "هاب مبدا",
+        hubCode: "کد هاب",
+        hubName: "نام هاب",
+        route: "مسیر",
+        pelak: "پلاک",
+        description: "توضیحات",
+        productGroup: "گروه محصول",
+        timeUnit: "واحد",
+        vehicleNumber: "شماره پلاک",
+        phone: "شماره موبایل",
+        email: "پست الکترونیکی",
+        username: "نام کاربری",
+        nationalCode: "کد ملی",
+        postalCode: "کد پستی",
+        address: "آدرس",
+        telNumber: "شماره تلفن",
+        selectParentCustomer: "مشتری والد",
+        zipCode: "کد پستی",
+        parentClient: "مشتری والد",
+        personelCode: "کد پرسنلی",
+        priceListDate: "تاریخ نرخ نامه",
+        product: "محصول",
+        consignmentType: "نوع مرسوله",
+        classification: "نوع رده",
+        source: "مبدا",
+        destination: "مقصد",
+        type: "نوع",
+        priceList: "نرخ نامه",
+        selectThirdPartyCategory: "گروه شخصیت",
+        customerSegments: "مشتری ها",
+        saleschannels: "کانال فروش",
+        serviceDeliveryCustomers: "گروه مشتری",
+        service: "سرویس",
+        fromCountryDevision: 'مبدا',
+        toCountryDevision: 'مقصد'
+
+    };
+
+    useEffect(() => {
+        const convertObject = Object.entries(filterData).map(([key, value], index) =>
+            value
+                ? {
+                    search: {[`${key}`]: value},
+                    id: index,
+                    mainTitle: key,
+                    title: persianName[key],
+                    // @ts-ignore
+                    value: value?.label
+                        // @ts-ignore
+                        ? value.label
+                        // @ts-ignore
+                        : value?.text
+                            // @ts-ignore
+                        ? value.text
+                            // @ts-ignore
+                        : value?.day
+                                // @ts-ignore
+                        ? value.year + "/" + value.month + "/" + value.day
+                        : Array.isArray(value)
+                        ? value.map((item) => item.text)
+                        : value,
+                }
+                : ""
+        );
+        setChipData(convertObject.filter((item) => item));
+    }, [filterData]);
+
+
+    // @ts-ignore
     return (
         <>
-            {console.log(chipData)}
+
             <div className='flex justify-start items-center gap-3 mt-6'>
-                {
-                    chipData && chipData.map((item, index) =>
-                    item.data &&
-                    <div key={index} className='bg-grayLight flex w-fit py-1 px-3 justify-between items-center rounded-md'>
-                        <span>{`${item.label} :`} {item.data}</span>
-                        <span className='mr-2 cursor-pointer' onClick={() => {
-                            handelClick(item, index)
-                        }}><BiXCircle/></span>
-                    </div>
-                        // <Chip title={item.label} value={item.data} key={index}/>
+                {chipData
+                    .filter(
+                        (item: { mainTitle: string; }) =>
+                            item?.mainTitle !== "isActive" &&
+                            item.mainTitle !== "vehicleNumber0" &&
+                            item.mainTitle !== "vehicleNumber1" &&
+                            item.mainTitle !== "vehicleNumber2" &&
+                            item.mainTitle !== "vehicleNumber3"
                     )
-                }
+                    .map(({ id, title, value }:any) => (
+                        <div key={id}
+                             className='bg-grayLight flex w-fit py-1 px-3 justify-between items-center rounded-md'>
+                            <span>{title} : {value}</span>
+                            <span className='mr-2 cursor-pointer' onClick={() => {
+                                handleRemoveChipData( id)
+                            }}><BiXCircle/></span>
+                        </div>
+                    ))}
+
                 {chipData.length > 0 ? (
                     <div>
-                    <button
-                        className='w-122 h-21 flex justify-center items-center flex-row-reverse border-none text-tomato'
-                        onClick={() => {
-                            setChipData([])
-                            formData.resetForm({});
-                        }}>حذف جستجوها <span><BiX/></span></button>
-                </div>) : null}
+                        <button
+                            className='w-122 h-21 flex justify-center items-center flex-row-reverse border-none text-tomato'
+                            onClick={() => {
+                                setChipData([])
+                                formData.resetForm({});
+                            }}>حذف جستجوها <span><BiX/></span></button>
+                    </div>) : null}
             </div>
 
 
--- src/pages/ServiceManagement/ServiceDefinition/view/SearchForm.tsx
+++ src/pages/ServiceManagement/ServiceDefinition/view/SearchForm.tsx
@@ -3,22 +3,20 @@ import {BiSearch, BiX, BiChevronDown} from 'react-icons/bi';
 import {useFormik} from "formik";
 import {useDispatch} from "react-redux";
 import Chip from "../../../../global/Chip/Chip";
-import PerfesionalSearch from "../../../../components/PerfesionalSearch/PerfesionalSearch";
+
 import {ServiceData} from "../../../../redux/ServiceDefine/ServiceDefineReducer";
 import AutocompleteInput from "../../../../global/Autocomplete/AutocompleteInput";
+import PerfesionalSearch from "./PerfesionalSearch";
+import {GetDataParams} from "../../../../services/Service_call";
+import {apiRoute} from "../../../../services/apiRoute";
 
 
 
-interface chipData {
-    label?: string,
-    data?: any,
-    name: string,
-}
-const SearchForm = () => {
+
+const SearchForm = () : JSX.Element=> {
     const dispatch = useDispatch()
-    const [options, setOptions] = useState<any>([])
-    const [chipData, setChipData] = useState<chipData[]>([])
-    const [search, setSearch] = useState({})
+    const [serviceCodeOptions, setServiceCodeOptions] = useState<any>([])
+    const [filterData, setFilterData] = useState({})
     const formik = useFormik({
         enableReinitialize: true,
         initialValues: {
@@ -26,28 +24,37 @@ const SearchForm = () => {
             name: "",
         },
         onSubmit: (values) => {
-            setChipData([
-                {
-                    label: "کد ",
-                    data: values.code,
-                    name: 'code'
-                },
-                {
-                    label: " نام",
-                    data: values.name,
-                    name: 'name'
-                }
-            ])
-            setSearch(values);
+            setFilterData(values);
         },
     });
 
     useEffect(() => {
         // @ts-ignore
         dispatch(ServiceData(formik.values))
-    }, [chipData.length])
-
-
+    }, [filterData])
+     const data= [
+         {id:1,text:'product'},
+         {id:2,text:'price'},
+         {id:3,text:'vemdor'},
+     ]
+    const handleChangeCode = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+        formik.setFieldValue(name, e.target.value)
+        const filterData=data.filter(item=>item.text.includes(e.target.value))
+        setServiceCodeOptions(filterData.map(item=> {
+            return {
+                label:item?.text
+            }}))
+        //mr hash please dont delete this comment//
+        // const params = `${e.target.value}`;
+        // setOptions(data.filter(item=>item.text.includes(e.target.value)))
+        // GetDataParams(apiRoute().get.GET_SERVICES+ params)
+    }
+    const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+        formik.setFieldValue(name, e.target.value)
+    }
+    const handleSelect = (val: any, name: string) => {
+        formik.setFieldValue(name, val)
+    }
     return (
         <>
             <div className='flex justify-start items-center mt-6 gap-4 flex-wrap'>
@@ -56,31 +63,19 @@ const SearchForm = () => {
                         <div className='Max-sm:mb-3'>
                             <AutocompleteInput
                                 label={"کد"}
-                                items={options}
+                                items={serviceCodeOptions}
                                 value={formik.values.code}
-                                onChange={(e: { target: { value: React.SetStateAction<string | null | undefined>; }; }) => {
-                                    // const params = `${e.target.value}`;
-                                    // GetDataParams(apiRoute().get.GET_SERVICES+ params)
-                                    formik.setFieldValue('code', e.target.value)
-                                }}
-                                onSelect={(val: React.SetStateAction<string | null | undefined>) => formik.setFieldValue('code', val)}
+                                onChange={(e) => handleChangeCode(e, 'code')}
+                                onSelect={(val: any) => handleSelect(val, 'code')}
                             />
                         </div>
                         <div>
                             <AutocompleteInput
                                 label={"عنوان"}
-                                items={[
-                                    {label: 'apple'},
-                                    {label: 'banana'},
-                                    {label: 'pear'}
-                                ]}
+                                items={[]}
                                 value={formik.values.name}
-                                onChange={
-                                    (e: { target: { value: React.SetStateAction<string | null | undefined>; }; }) => {
-                                        formik.setFieldValue('name', e.target.value)
-                                    }
-                                }
-                                onSelect={(val: React.SetStateAction<string | null | undefined>) => formik.setFieldValue('name', val)}
+                                onChange={(e) => handleChangeName(e, 'name')}
+                                onSelect={(val: any) => handleSelect(val, 'name')}
                             />
                             {/*<InputIcon text='عنوان' handleOnSelect={handleOnSelect} handleOnSearch={setName}/>*/}
                         </div>
@@ -89,11 +84,12 @@ const SearchForm = () => {
                             <span className='ml-2'><BiSearch size={20}/></span>جستجو
                         </button>
                     </div>
+
                 </form>
-                {/*<PerfesionalSearch text="جستجوی پیشرفته" LeftIcon={<BiChevronDown/>}/>*/}
+                <PerfesionalSearch formData={formik} text="جستجوی پیشرفته" LeftIcon={<BiChevronDown/>}/>
             </div>
             {/* list of chip */}
-            {search && <Chip chipData={chipData} setChipData={setChipData} formData={formik}/>}
+            {filterData && <Chip filterData={filterData} formData={formik}/>}
         </>
     )
 }
