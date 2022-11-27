6279948c2ba2138072d78ab0c313deea646f2952
hasanb69 <hasanbaratiasp@gmail.com>
2022-11-27 15:47:25 +0330

    adnvanced  search completed

--- src/components/PerfesionalSearch/PerfesionalSearch.tsx
+++ src/components/PerfesionalSearch/PerfesionalSearch.tsx
@@ -1,94 +1,52 @@
-import { useState } from 'react';
-import InputText from './../../global/InputText/InputText';
-import Select from "react-select"
-import {
-  Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-  Button,
-} from "@material-tailwind/react";
+import { useState, useEffect, useRef } from "react";
 
-import { BiChevronDown ,BiXCircle} from 'react-icons/bi';
-import { BiCog,BiSearch } from "react-icons/bi";
-import CustomizeModal from './CustomizeModal';
-import SimpleButton from '../../global/SimpleButton/SimpleButton';
-const PerfesionalSearch = ({LeftIcon,text}:{LeftIcon?:JSX.Element,text?:string}) => {
-   
-const [costomizeModal,setCostomizeModal]=useState(false)
-const handelClick=()=>{
-  setCostomizeModal(prev=>!prev)
+import Select from "react-select";
+import { Popover, PopoverHandler, PopoverContent, Button } from "@material-tailwind/react";
+import { BiChevronDown, BiCog } from "react-icons/bi";
+import SimpleButton from "../../global/SimpleButton/SimpleButton";
+
+
+interface propsData {
+  formData?: any;
+  text?: string;
+  LeftIcon?: JSX.Element;
+  children?: JSX.Element;
 }
 
-
+const PerfesionalSearch = ({ LeftIcon, text, formData, children }: propsData): JSX.Element => {
   return (
     <div className="w-160 relative">
-
-<Popover
-placement="bottom"
-      animate={{
-        mount: { scale: 1, y: 0 },
-        unmount: { scale: 0, y: 25 },
-      }}
-    >
-      <PopoverHandler>
-        <Button className='border-none bg-transparent text-dark shadow-none flex justify-around items-center'>جستجوی پیشرفته <span className='mr-5'><BiChevronDown/></span><span></span></Button>
-      </PopoverHandler>
-      <PopoverContent>
-      <div className=" " style={{width:"550px"}}>
-       <div className='flex flex-wrap justify-between items-center gap-4'>
-        <div><InputText/></div>
-        <div> <Select className='simple_select' data-before="text"/></div>
-        <div> <Select className='simple_select'/></div>
-        <div> <Select className='simple_select'/></div>
-       </div>
-
-
-
-       <div className='flex justify-between items-center mt-5'>
-       <SimpleButton text='شخصی سازی' icon={<BiCog color='gray'/>} handelClick={handelClick}/>
-       <SimpleButton text='پاک کردن ورودی ها'/>
-       <SimpleButton className='full-tomato-btn' text='جستجو'/>
-
-       </div>
-      </div>
-      </PopoverContent>
-    </Popover>
-
-
-
-
-    {/* costom modal  */}
-    <Dialog open={costomizeModal} handler={()=>setCostomizeModal(prev=>!prev)}>
-        <DialogHeader>
-          <div className='flex justify-between items-center w-full'>
-          <span className='text-16'>شخصی سازی</span>
-            <button className='border-none focus:border-none' onClick={()=>setCostomizeModal(prev=>!prev)}><BiXCircle/></button>
-           </div>
-        </DialogHeader>
-        <DialogBody divider>
-          {/* costomize modal */}
-         <CustomizeModal/>
-        </DialogBody>
-        <DialogFooter>
-          <Button
-           
-            onClick={()=>setCostomizeModal(prev=>!prev)}
-            className="ml-3 bg-secondaryColor text-dark border-none"
-          >
-            <span>لغو</span>
+      <Popover
+        placement="bottom"
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0, y: 25 },
+        }}
+      >
+        <PopoverHandler>
+          <Button className="border-none bg-transparent text-dark shadow-none flex justify-around items-center">
+            جستجوی پیشرفته{" "}
+            <span className="mr-5">
+              <BiChevronDown />
+            </span>
+            <span></span>
           </Button>
-          <Button className='bg-tomato border-none'  onClick={()=>setCostomizeModal(prev=>!prev)}>
-            <span>ذخیره</span>
-          </Button>
-        </DialogFooter>
-      </Dialog>
-   {/*end costom modal  */}
-  </div>
-  )
-}
+        </PopoverHandler>
+        <PopoverContent>
+          <div className="" style={{ width: "550px" }}>
+            <div className="flex  justify-between items-center gap-4">
+              {children}
+              </div>
+            <div className="flex justify-between items-center mt-5">
+              <SimpleButton text="شخصی سازی" icon={<BiCog color="gray" />} />
+              <SimpleButton text="پاک کردن ورودی ها" />
+              <SimpleButton handelClick={()=>formData.handleSubmit()}  className="full-tomato-btn" text="جستجو" />
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
 
-export default PerfesionalSearch
+export default PerfesionalSearch;
--- src/pages/ServiceManagement/ServiceDefinition/view/SearchForm.tsx
+++ src/pages/ServiceManagement/ServiceDefinition/view/SearchForm.tsx
@@ -1,97 +1,111 @@
-import React, {useEffect, useState} from 'react'
-import {BiSearch, BiX, BiChevronDown} from 'react-icons/bi';
-import {useFormik} from "formik";
-import {useDispatch} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { BiSearch, BiX, BiChevronDown } from "react-icons/bi";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import Select from "react-select";
 import Chip from "../../../../global/Chip/Chip";
-
-import {ServiceData} from "../../../../redux/ServiceDefine/ServiceDefineReducer";
+import { ServiceData } from "../../../../redux/ServiceDefine/ServiceDefineReducer";
 import AutocompleteInput from "../../../../global/Autocomplete/AutocompleteInput";
-import PerfesionalSearch from "./PerfesionalSearch";
-import {GetDataParams} from "../../../../services/Service_call";
-import {apiRoute} from "../../../../services/apiRoute";
+import { GetDataParams } from "../../../../services/Service_call";
+import { apiRoute } from "../../../../services/apiRoute";
+import SimpleButton from "../../../../global/SimpleButton/SimpleButton";
+import PerfesionalSearch from "../../../../components/PerfesionalSearch/PerfesionalSearch";
 
+const SearchForm = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const [serviceCodeOptions, setServiceCodeOptions] = useState<any>([]);
+  const [filterData, setFilterData] = useState({});
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      code: "",
+      name: "",
+    },
+    onSubmit: (values) => {
+      setFilterData(values);
+    },
+  });
 
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(ServiceData(formik.values));
+  }, [filterData]);
+  const data = [
+    { id: 1, text: "product" },
+    { id: 2, text: "price" },
+    { id: 3, text: "vemdor" },
+  ];
+  const handleChangeCode = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+    formik.setFieldValue(name, e.target.value);
+    const filterData = data.filter((item) => item.text.includes(e.target.value));
+    setServiceCodeOptions(
+      filterData.map((item) => {
+        return {
+          label: item?.text,
+        };
+      })
+    );
+    //mr hash please dont delete this comments//
+    // const params = `${e.target.value}`;
+    // setOptions(data.filter(item=>item.text.includes(e.target.value)))
+    // GetDataParams(apiRoute().get.GET_SERVICES+ params)
+  };
+  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+    formik.setFieldValue(name, e.target.value);
+  };
+  const handleSelect = (val: any, name: string) => {
+    formik.setFieldValue(name, val);
+  };
+  return (
+    <>
+      <div className="flex justify-start items-center mt-6 gap-4 flex-wrap">
+        <form onSubmit={formik.handleSubmit}>
+          <div className=" flex gap-3 justify-start items-center flex-wrap">
+            <AutocompleteInput
+              label={"کد"}
+              items={serviceCodeOptions}
+              value={formik.values.code}
+              onChange={(e) => handleChangeCode(e, "code")}
+              onSelect={(val: any) => handleSelect(val, "code")}
+            />
+            <AutocompleteInput
+              label={"عنوان"}
+              items={[]}
+              value={formik.values.name}
+              onChange={(e) => handleChangeName(e, "name")}
+              onSelect={(val: any) => handleSelect(val, "name")}
+            />
+            {/*<InputIcon text='عنوان' handleOnSelect={handleOnSelect} handleOnSearch={setName}/>*/}
+            <SimpleButton className="full-gray-btn" icon={<BiSearch size={20} />} text="جستجو" />
+          </div>
+        </form>
+        <PerfesionalSearch formData={formik} text="جستجوی پیشرفته" LeftIcon={<BiChevronDown />}>
+          <>
+            <Select
+              name="selectBagType"
+              className="simple_select"
+              placeholder="محصول"
+              options={[
+                { label: "محصول اول", value: 1 },
+                { label: "محصول دوم", value: 2 },
+              ]}
+              onChange={(value) => {
+                formik.setFieldValue("product", { id: value?.value, text: value?.label });
+              }}
+              value={{
+                // @ts-ignore
+                value: formik.values?.product?.id,
+                // @ts-ignore
+                label: formik.values?.product?.text,
+              }}
+            />
+          </>
+        </PerfesionalSearch>
+      </div>
+      {/* list of chip */}
+      {filterData && <Chip filterData={filterData} formData={formik} />}
+    </>
+  );
+};
 
-
-const SearchForm = () : JSX.Element=> {
-    const dispatch = useDispatch()
-    const [serviceCodeOptions, setServiceCodeOptions] = useState<any>([])
-    const [filterData, setFilterData] = useState({})
-    const formik = useFormik({
-        enableReinitialize: true,
-        initialValues: {
-            code: "",
-            name: "",
-        },
-        onSubmit: (values) => {
-            setFilterData(values);
-        },
-    });
-
-    useEffect(() => {
-        // @ts-ignore
-        dispatch(ServiceData(formik.values))
-    }, [filterData])
-     const data= [
-         {id:1,text:'product'},
-         {id:2,text:'price'},
-         {id:3,text:'vemdor'},
-     ]
-    const handleChangeCode = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
-        formik.setFieldValue(name, e.target.value)
-        const filterData=data.filter(item=>item.text.includes(e.target.value))
-        setServiceCodeOptions(filterData.map(item=> {
-            return {
-                label:item?.text
-            }}))
-        //mr hash please dont delete this comment//
-        // const params = `${e.target.value}`;
-        // setOptions(data.filter(item=>item.text.includes(e.target.value)))
-        // GetDataParams(apiRoute().get.GET_SERVICES+ params)
-    }
-    const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
-        formik.setFieldValue(name, e.target.value)
-    }
-    const handleSelect = (val: any, name: string) => {
-        formik.setFieldValue(name, val)
-    }
-    return (
-        <>
-            <div className='flex justify-start items-center mt-6 gap-4 flex-wrap'>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className=' flex gap-3 justify-start items-center flex-wrap'>
-                        <div className='Max-sm:mb-3'>
-                            <AutocompleteInput
-                                label={"کد"}
-                                items={serviceCodeOptions}
-                                value={formik.values.code}
-                                onChange={(e) => handleChangeCode(e, 'code')}
-                                onSelect={(val: any) => handleSelect(val, 'code')}
-                            />
-                        </div>
-                        <div>
-                            <AutocompleteInput
-                                label={"عنوان"}
-                                items={[]}
-                                value={formik.values.name}
-                                onChange={(e) => handleChangeName(e, 'name')}
-                                onSelect={(val: any) => handleSelect(val, 'name')}
-                            />
-                            {/*<InputIcon text='عنوان' handleOnSelect={handleOnSelect} handleOnSearch={setName}/>*/}
-                        </div>
-                        <button type='submit'
-                                className='w-160 h-40 flex justify-center items-center bg-lightesGray border-none rounded-lg text-md relative active:-top-3 text-halfDark'>
-                            <span className='ml-2'><BiSearch size={20}/></span>جستجو
-                        </button>
-                    </div>
-
-                </form>
-                <PerfesionalSearch formData={formik} text="جستجوی پیشرفته" LeftIcon={<BiChevronDown/>}/>
-            </div>
-            {/* list of chip */}
-            {filterData && <Chip filterData={filterData} formData={formik}/>}
-        </>
-    )
-}
-
-export default SearchForm
+export default SearchForm;
