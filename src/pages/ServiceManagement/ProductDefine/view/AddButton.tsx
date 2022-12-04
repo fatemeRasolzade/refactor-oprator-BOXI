e904892c8a6615d1750dcd612933620b62d1d506
hasanb69 <hasanbaratiasp@gmail.com>
2022-12-04 16:28:29 +0330

    update product define addbutton component

---
+++ src/pages/ServiceManagement/ProductDefine/view/AddButton.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { Dialog } from "@material-tailwind/react";
+import { GrFormClose } from "react-icons/gr";
+import ModalOperation from "../../../../pages/Roles/view/ModalOperation";
+import ActionForms from "./ActionsForm";
+interface PropAddButton {
+  text?: string;
+  subItemOne?: string;
+  subItemTwo?: string;
+  RightIcon?: JSX.Element;
+  LeftIcon?: JSX.Element;
+  color?: string;
+  textColor?: string;
+}
+
+const AddButton = ({
+  text = "افزودن",
+  subItemOne,
+  subItemTwo,
+  RightIcon,
+  LeftIcon,
+  color,
+  textColor,
+}: PropAddButton) => {
+  const [toggle, setToggle] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [mode, setMode] = useState(null);
+  const handelClick = () => {
+    setToggle(false);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <div className="w-160 relative">
+      <button className={`btn  full-tomato-btn`} onClick={() => setToggle(!toggle)}>
+        <span>{LeftIcon}</span> <span className="text-lg">{text}</span> <span>{RightIcon}</span>
+      </button>
+      {toggle ? (
+        <div className="w-full bg-red  absolute  right-0 shadow-lg rounded-md ">
+          <ul>
+            <li>
+              <button
+                className="w-full py-2 px-3 border-none hover:bg-gray-200 text-sm text-right "
+                onClick={handelClick}
+              >
+                افزودن
+                {/* {subItemOne} */}
+              </button>
+            </li>
+            <li>
+              <button className="w-full py-2 px-3 border-none hover:bg-gray-200 text-right" onClick={handelClick}>
+                {subItemTwo}
+              </button>
+            </li>
+          </ul>
+        </div>
+      ) : null}
+
+       {/* {isModalOpen && <ActionForms/>} */}
+      {/* <Dialog open={isModalOpen} handler={setIsModalOpen} size={'md'}>
+        <button
+          className="flex w-[50px] h-[50px]  border-none items-center justify-center"
+          onClick={() => setIsModalOpen(false)}
+        >
+          <GrFormClose />
+        </button>
+        <div className="p-5">
+
+        <ActionForms  setIsModalOpen={setIsModalOpen}  isModalOpen={isModalOpen}/>
+        </div>
+        
+      </Dialog> */}
+    </div>
+  );
+};
+
+export default AddButton;
