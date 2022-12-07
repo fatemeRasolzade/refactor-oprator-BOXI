541eb0d224eac920e88b0defb363a2e046d11f80
faridhash76 <kingfarid76@gmail.com>
2022-12-07 17:24:31 +0330

    refactor changes in add edit personell

--- src/pages/Personnel/view/AddEditPerson.tsx
+++ src/pages/Personnel/view/AddEditPerson.tsx
@@ -84,7 +84,7 @@ const AddEditPerson: FC<AddEditPersonProps> = ({ currentData }) => {
       <Dialog
         open={isModalOpen}
         handler={setIsModalOpen}
-        className="min-w-[1100px]"
+        className="min-w-[1150px]"
       >
         <button
           className="flex w-[50px] h-[50px]  border-none items-center justify-center"
@@ -95,11 +95,8 @@ const AddEditPerson: FC<AddEditPersonProps> = ({ currentData }) => {
         <h3 className="flex w-full justify-center text-gray-700 font-bold text-lg">
           {currentData ? "ویرایش پرسنل" : "افزودن پرسنل"}
         </h3>
-        <form
-          onSubmit={handleSubmit}
-          className="flex w-full flex-col items-center gap-6 mb-6 p-6"
-        >
-          <div className="flex w-[90%] justify-start align-center gap-x-3">
+        <form onSubmit={handleSubmit} className="grid grid-cols-4 gap-6  p-6 ">
+          {/* <div className="flex w-[90%] justify-start align-center gap-x-3">
             <InputText
               className="w-[33%]"
               label="کد پرسنلی"
@@ -190,6 +187,128 @@ const AddEditPerson: FC<AddEditPersonProps> = ({ currentData }) => {
             />
           </div>
           <div className="flex w-[80%] justify-center gap-x-12">
+            <SimpleButton
+              type="submit"
+              text="بله"
+              className="full-tomato-btn px-[90px] "
+            />
+            <SimpleButton
+              type="button"
+              text="خیر"
+              className="full-lightTomato-btn px-[90px]"
+              handelClick={() => {
+                setIsModalOpen(false);
+              }}
+            />
+          </div> */}
+          <div className=" ">
+            <InputText
+              className="w-full"
+              label="کد پرسنلی"
+              name="personelCode"
+              handleChange={formik.handleChange}
+              values={formik.values.personelCode}
+              important
+              type={"text"}
+            />
+          </div>
+          <div className=" ">
+            <InputText
+              className="w-full"
+              label="کد ملی"
+              name="nationalCode"
+              handleChange={formik.handleChange}
+              values={formik.values.nationalCode}
+              important
+              type={"text"}
+            />
+          </div>
+          <div className="col-span-2">
+            <InputText
+              label="نام و نام خانوادگی"
+              name="name"
+              handleChange={formik.handleChange}
+              values={formik.values.name}
+              important
+              type={"text"}
+            />
+          </div>
+          <div className="col-span-1 ">
+            <InputText
+              className="w-full"
+              label="شماره موبایل"
+              name="mobile"
+              handleChange={formik.handleChange}
+              values={formik.values.mobile}
+              important
+              type={"text"}
+            />
+          </div>
+          <div className="col-span-1 ">
+            <InputText
+              className="w-full"
+              label="پست الکترونیک"
+              name="email"
+              handleChange={formik.handleChange}
+              values={formik.values.email}
+              type={"text"}
+            />
+          </div>
+          <div className="col-span-2 h-[40px] mb-[20px]">
+            <CustomSwitch
+              active={true}
+              handleChange={(value: boolean) =>
+                setFieldValue("isActive", value)
+              }
+            />
+          </div>
+          <InputText
+            label="نام کاربری"
+            name="username"
+            handleChange={formik.handleChange}
+            values={formik.values.username}
+            important
+            type={"text"}
+          />
+          {!currentData && (
+            <>
+              <div className="col-span-1 ">
+                <InputText
+                  label=" گذر واژه"
+                  name="password"
+                  handleChange={formik.handleChange}
+                  values={formik.values.password}
+                  important
+                  type={"password"}
+                />
+              </div>
+              <div className="col-span-1 ">
+                <InputText
+                  className="w-full"
+                  label=" تایید گذر واژه"
+                  name="confirmPassword"
+                  handleChange={formik.handleChange}
+                  values={formik.values.confirmPassword}
+                  important
+                  type={"password"}
+                />
+              </div>
+            </>
+          )}
+
+          <div className="col-span-1 ">
+            <InputText
+              className="w-full"
+              label="سوپر ادمین"
+              name="confirmPassword"
+              handleChange={formik.handleChange}
+              values={formik.values.confirmPassword}
+              important
+              type={"text"}
+            />
+          </div>
+          <div className="col-span-2" />
+          <div className="col-span-2 flex gap-4 w-full justify-end">
             <SimpleButton
               type="submit"
               text="بله"
