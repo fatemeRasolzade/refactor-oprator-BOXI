2858a0845b0dfa48d424eb54a77723c2edccb104
hasanb69 <hasanbaratiasp@gmail.com>
2022-12-10 14:33:22 +0330

    change userservice name

---
+++ src/pages/ServiceManagement/ProductDefine/view/serviceProvisionData.js
@@ -0,0 +1,57 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { useEffect, useState } from "react";
+import { Base_url4 } from "../../../../services/apiRoute";
+import UserService from "../../../../services/userService";
+import { apiRoute } from "../../../../services/apiRoute";
+import { selectDataFromServer } from "../../../../services/Service_call";
+console.log("run");
+// export const serviceProvisionData = createApi({
+//   reducerPath: "service",
+
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: Base_url4,
+//   }),
+//   prepareHeaders: (headers) => {
+//     const token = localStorage.getItem("userToken");
+//     console.log(token, "token");
+//     // If we have a token set in state, let's assume that we should be passing it.
+//     if (token) {
+//       headers.set("Authorization", `Bearer ${token}`);
+//     }
+//     return headers;
+//   },
+// //   endpoints: (builder) => ({
+// //     updateUser: builder.query({
+// //       query: () => ({
+// //         url: `/core-api/vendor/select?filter=`,
+// //         method: 'GET',
+// //         // headers: {
+// //         //     'content-type': 'text/plain',
+// //         // },
+// //       }),
+// //     }),
+// // })
+//   endpoints: (builder) => ({
+//     getService: builder.query({
+//       query: () => `/core-api/vendor/select?filter=`,
+//     }),
+//   }),
+// })
+
+// export const { useGetServiceQuery } = serviceProvisionData;
+
+export const useGetOptions = () => {
+  const [options, setOptions] = useState([]);
+  useEffect(() => {
+    try {
+      selectDataFromServer(apiRoute().get.GET_PRODUCT_GROUPS).then((res) => {
+        if (res.status === "OK") setOptions(res?.payload?.content);
+      });
+      // getDataFromServer(apiRoute().get.select_hub_category).then(res=>{if(res.status==="OK") setCatHub(res.payload.content)})
+    } catch (error) {
+      // ErrorAlert("دریافت دیتا با خطلا مواجه شد");
+    }
+  }, []);
+
+  return { options };
+};
--- src/services/http_service.js
+++ src/services/http_service.js
@@ -1,26 +1,22 @@
 import axios from "axios";
 import UserService from "./userService";
 
+axios.defaults.baseURL = "http://boxi.local:40000/";
 
+const configure = () => {
+  axios.interceptors.request.use((config) => {
+    if (UserService.isLoggedIn()) {
+      const cb = () => {
+        config.headers.Authorization = `Bearer ${localStorage.getItem("myToken")}`;
+        return Promise.resolve(config);
+      };
+      return UserService.updateToken(cb);
+    }
+  });
+};
 
-	axios.defaults.baseURL = "http://boxi.local:40000/";
-
- const configure = () => {
-	axios.interceptors.request.use((config) => {
-			
-		if (UserService.isLoggedIn()) {
-		  const cb = () => {
-			config.headers.Authorization = `Bearer ${localStorage.getItem("myToken")}`;
-			return Promise.resolve(config);
-		  };
-		  return UserService.updateToken(cb);
-		}
-	  });
-}
-
-
-export default{
-  config:configure,
+export default {
+  config: configure,
   get: axios.get,
   post: axios.post,
   put: axios.put,
