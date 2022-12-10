03373dc34b66f90f13b02969ca2bbf0ffe42f795
hasanb69 <hasanbaratiasp@gmail.com>
2022-12-10 09:18:46 +0330

    merge with dev

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,4 @@
 //public client
-import axios from "axios";
 import keycloak from "keycloak-js";
 const _kc=new keycloak({
 	"url":"http://boxi.local:8080",
@@ -10,6 +9,23 @@ const _kc=new keycloak({
 	"clientId":"react-client",
 	"auth-server-url":"http://boxi.local:8080"
 });
+
+
+
+const initKeycloak = (onAuthenticatedCallback) => {
+	_kc.init({
+		onLoad: 'login-required',
+		//onLoad: 'check-sso'
+
+	})
+		.then((authenticated) => {
+			if (authenticated) {
+			onAuthenticatedCallback();
+			} else {
+			  doLogin();
+			}
+		})
+};
 
 const doLogin = _kc.login;
 
@@ -25,29 +41,6 @@ const updateToken = (successCallback) =>{
 		.then(successCallback)
 		.catch(doLogin);
 }
-
-
-
-
-const initKeycloak = (onAuthenticatedCallback) => {
-	_kc.init({
-		onLoad: 'login-required',
-		checkLoginIframe: false,
-		
-		//onLoad: 'check-sso'
-
-	})
-		.then((authenticated) => {
-			if (authenticated) {
-				console.log('token')
-				axios.defaults.headers.common["Authorization"] = "Bearer " + getToken();
-				window.localStorage.setItem("myToken",getToken())
-			onAuthenticatedCallback();
-			} else {
-			  doLogin();
-			}
-		})
-};
 
 
 const getUsername = () => _kc.tokenParsed?.preferred_username;
