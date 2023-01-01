2c6543debad1a51fa56f9199faa26854c95e97a6
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-01-01 10:08:48 +0330

    finish StatusBar component

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -1,25 +1,79 @@
-interface StatusBarProps {
-  options: Array<any>;
+import { BsCartCheck, BsFillGridFill, BsMinecartLoaded, BsUpcScan } from "react-icons/bs";
+import { MdOutlineCallMissed, MdOutlineTimer, MdOutlineTimerOff } from "react-icons/md";
+import { IoMdCheckboxOutline } from "react-icons/io";
+import { GiSwapBag } from "react-icons/gi";
+import { ImEnter } from "react-icons/im";
+import { FiXSquare } from "react-icons/fi";
+
+interface OptionsProps {
+  name: string;
 }
 
-const iconList = {
-  All: "همه",
-  Planned: "برنامه ریزی شده",
-  NotPlanned: "برنامه ریزی نشده",
-  InTransit: "در حال حمل",
-  SuccessfulDelivery: "تحویل موفق",
-  UnsuccessfulDelivery: "تحویل ناموفق",
-  ScanInHub: "اسکن در هاب",
-  Bagged: "کیسه شده",
-  Loaded: "بارگیری شده",
-  EntranceToHub: "ورودی به هاب",
-  BackToOrigin: "بازگشتی به مبدا",
+interface StatusBarProps {
+  Options: Array<OptionsProps>;
+}
+
+const Elements: any = {
+  All: {
+    icon: <BsFillGridFill size={32} className="text-[#F59E0B]" />,
+    text: "همه",
+  },
+  Planned: {
+    icon: <MdOutlineTimer size={30} className="text-[#06B6D4]" />,
+    text: "برنامه ریزی شده",
+  },
+  NotPlanned: {
+    icon: <MdOutlineTimerOff size={30} className="text-[#EC4899]" />,
+    text: "برنامه ریزی نشده",
+  },
+  InTransit: {
+    icon: <BsCartCheck size={30} className="text-[#0EA5E9]" />,
+    text: "در حال حمل",
+  },
+  SuccessfulDelivery: {
+    icon: <IoMdCheckboxOutline size={30} className="text-[#10B981]" />,
+    text: "تحویل موفق",
+  },
+  UnsuccessfulDelivery: {
+    icon: <FiXSquare size={30} className="text-[#F43F5E]" />,
+    text: "تحویل ناموفق",
+  },
+  ScanInHub: {
+    icon: <BsUpcScan size={30} className="text-[#EAB308]" />,
+    text: "اسکن در هاب",
+  },
+  Bagged: {
+    icon: <GiSwapBag size={30} className="text-[#A16207]" />,
+    text: "کیسه شده",
+  },
+  Loaded: {
+    icon: <BsMinecartLoaded size={30} className="text-[#84CC16]" />,
+    text: "بارگیری شده",
+  },
+  EntranceToHub: {
+    icon: <ImEnter size={30} className="text-[#9CA3AF]" />,
+    text: "ورودی به هاب",
+  },
+  BackToOrigin: {
+    icon: <MdOutlineCallMissed size={30} className="text-[#3B82F6]" />,
+    text: "بازگشتی به مبدا",
+  },
 };
 
-const textList = {};
-
-const StatusBar = ({ options }: StatusBarProps) => {
-  return <div>StatusBar</div>;
+const StatusBar = ({ Options }: StatusBarProps) => {
+  return (
+    <div className="flex-start-start gap-7">
+      {Options.map((item: any) => (
+        <div className="flex-start-center gap-2">
+          <div>{Elements[item.name].icon}</div>
+          <div className="flex-between-start flex-col ">
+            <p className="!text-darkGray">{item.value}</p>
+            <p className="!text-darkGray">{Elements[item.name].text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default StatusBar;
--- src/pages/ConsignmentManage/ConsignmentManage.tsx
+++ src/pages/ConsignmentManage/ConsignmentManage.tsx
@@ -1,7 +1,20 @@
-import NotFound from "../../components/NotFound/NotFound";
+import StatusBar from "../../components/StatusBar/StatusBar";
 
 const ConsignmentManage = () => {
-  return <NotFound />;
+  const Options = [
+    { name: "All", value: 1000 },
+    { name: "Planned", value: 1000 },
+    { name: "NotPlanned", value: 1000 },
+    { name: "InTransit", value: 1000 },
+    { name: "SuccessfulDelivery", value: 1000 },
+    { name: "UnsuccessfulDelivery", value: 1000 },
+    { name: "ScanInHub", value: 1000 },
+    { name: "Bagged", value: 1000 },
+    { name: "Loaded", value: 1000 },
+    { name: "EntranceToHub", value: 1000 },
+    { name: "BackToOrigin", value: 1000 },
+  ];
+  return <StatusBar Options={Options} />;
 };
 
 export default ConsignmentManage;
