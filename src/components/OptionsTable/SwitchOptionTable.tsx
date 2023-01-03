4c6127a7fdf7ff146712ef86860110fc1b574537
faridhash76 <kingfarid76@gmail.com>
2023-01-03 09:19:21 +0330

    adding the componentItem in models

--- src/components/OptionsTable/SwitchOptionTable.tsx
+++ src/components/OptionsTable/SwitchOptionTable.tsx
@@ -4,8 +4,7 @@ import { GoDesktopDownload } from "react-icons/go";
 import { Link } from "react-router-dom";
 import AddButton from "../../global/addButton/AddButton";
 import SimpleButton from "../../global/SimpleButton/SimpleButton";
-import CustomSwitch from "../../global/Switch/Switch";
-import { ExportExcel } from "../../tools/functions/Methods";
+import { componentItem } from "../../models/SwitchOptionsModel";
 
 interface accessPageObject {}
 interface SwitchOptionTableProps {
@@ -24,43 +23,3 @@ const SwitchOptionTable: FC<SwitchOptionTableProps> = ({ accessPage }) => {
 };
 
 export default SwitchOptionTable;
-const componentItem: any = {
-  A1: {
-    JSXItem: (value: any) => (
-      <SimpleButton
-        handelClick={() => ExportExcel(value)}
-        text="خروجی اکسل"
-        icon={<GoDesktopDownload color="black" />}
-        className="centering rounded-lg text-black w-full"
-      />
-    ),
-    code: "A1",
-  },
-  A2: {
-    JSXItem: (value: any) =>
-      value.isToggle ? (
-        <AddButton ToggleOptions={value.ToggleOptions} />
-      ) : (
-        <>
-          <Link to={value.to}>
-            <SimpleButton
-              text="افزودن"
-              className="full-tomato-btn w-[160px] h-[40px] centering rounded-lg text-white"
-              icon={<BiPlus color="white" />}
-            />
-          </Link>
-        </>
-      ),
-    code: "A2",
-  },
-  A3: {
-    JSXItem: (value: any) => (
-      <>
-        <CustomSwitch active={value.data} handleChange={value.action} />
-      </>
-    ),
-    code: "A3",
-  },
-  A4: { JSXItem: () => <>group add </>, code: "A4" },
-  A5: { JSXItem: () => <>group add </>, code: "A5" },
-};
